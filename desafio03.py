e323054e819a29f5e7efa10ea2e7be2de903b4c7
Luciano <39633729+lportinari@users.noreply.github.com>
2019-03-17 08:04:19 -0300

    Add files via upload

---
+++ desafio03.py
@@ -0,0 +1,9 @@
+'''
+Crie um programa que leia dois números e mostre a soma entre deles
+'''
+
+#Revisão
+n1 = float(input('Me fale um número com virgula: '))
+n2 = float(input('Fale outro: '))
+s = (n1 + n2)
+print('A soma entre {} e {} é {}!'.format(n1, n2, s))
